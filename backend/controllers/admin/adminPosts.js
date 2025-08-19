f23a89370bc2f2307b8f2220ab71a92189deb8cd
Maaz Ahmad Khan <ma3073412@gmail.com>
2025-08-19 20:27:15 +0530

    fixing the adminPosts

--- backend/controllers/admin/adminPosts.js
+++ backend/controllers/admin/adminPosts.js
@@ -1,4 +1,5 @@
 const Post = require('../../models/user/Post');
+const PostDetails = require('../../models/user/PostDetails');
 
 // GET all posts (optional filter by status)
 const getAllPost = async (req, res) => {
@@ -14,6 +15,7 @@ const getAllPost = async (req, res) => {
 const deletePost = async  (req, res) => {
   try {
     await Post.findByIdAndDelete(req.params.id);
+    await PostDetails.findOneAndDelete({postid: req.params.id});
     res.json({ message: 'Post deleted successfully' });
   } catch (err) {
     res.status(500).json({ message: err.message });
